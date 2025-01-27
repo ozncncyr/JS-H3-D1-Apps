214c982d16a821f2fdd9751e5c2229738645d466
ozncncyr <ozncncyr@outlook.com>
2025-01-27 23:19:23 +0300

    Add letter grade calculation and user feedback to score calculator

--- puanHesapla.js
+++ puanHesapla.js
@@ -1,5 +1,3 @@
-alert("Üniversite Puan Hesaplayıcıya Hoşgeldiniz! :)"); // Karşılama mesajımız.
-
 function vizeHesapla(puan, agirlik) { // Vize Puanını Hesaplayan Fonksiyon
     return (puan * agirlik) / 100;
 }
@@ -14,6 +12,51 @@ function projeHesapla(puan, agirlik) { // Proje Puanını Hesaplayan Fonksiyon
 
 function genelToplam(vize, final, proje) { // Genel Toplam Puanı Hesaplayan Fonksiyon
     return vize + final + proje;
+}
+
+function harfNotuHesapla(puan) {
+    if (puan < 35) {
+        return "FF";
+    }
+    else if (puan >= 35 && puan < 50) {
+        return "FD";
+    }
+    else if (puan >= 50 && puan < 58) {
+        return "DD";
+    }
+    else if (puan >= 58 && puan < 65) {
+        return "DC";
+    }
+    else if (puan >= 65 && puan < 75) {
+        return "CC";
+    }
+    else if (puan >= 75 && puan < 80) {
+        return "CB";
+    }
+    else if (puan >= 80 && puan < 85) {
+        return "BB";
+    }
+    else if (puan >= 85 && puan < 90) {
+        return "BA";
+    }
+    else {
+        return "AA";
+    }
+}
+
+function notMetinUret(harf) {
+    switch (harf) {
+        case "AA": return "Harikasın!";
+        case "BA": return "Çok Başarılı!";
+        case "BB": return "Başarılı!";
+        case "CB": return "İyi!";
+        case "CC": return "Geçerli!";
+        case "DC": return "Koşullu Geçer!";
+        case "DD": return "Koşullu Geçer(Düşük)!";
+        case "FD": return "Başarısız!";
+        case "FF": return "Rezalet!";
+        default: return "";
+    }
 }
 
 function puanHesapla() { // Programı Çalıştıran Ana Fonksiyon
@@ -35,4 +78,23 @@ function puanHesapla() { // Programı Çalıştıran Ana Fonksiyon
 
     // Aldığımız vize, final ve proje puanları kendi ağırlıklarına göre hesaplandı ve genel toplama ulaşıyoruz.
     let genelToplamPuan = genelToplam(vizeToplam, finalToplam, projeToplam);
+
+    // Aldığımız Toplam Puana göre harf notu karşılığını kaydediyoruz.
+    let harfNotu = harfNotuHesapla(genelToplamPuan);
+
+    // Sahip olduğumuz harf notlarının metinsel bir karşlığını alıyoruz.
+    let notMetni = notMetinUret(harfNotu);
+
+    // Kullanıcıya sonuçları gösteriyoruz.
+    alert(`Ders Puan Bilgileriniz: \n${genelToplamPuan} - ${harfNotu} - ${notMetni}`);
+    if (confirm("Programı kullandığınız için teşekkürler. Devam etmek istiyor musunuz?")) {
+        puanHesapla();
+    }
+    else {
+        alert("Görüşmek Üzere :)");
+    }
 }
+
+alert("Üniversite Puan Hesaplayıcıya Hoşgeldiniz! :)"); // Karşılama mesajımız.
+
+puanHesapla();
